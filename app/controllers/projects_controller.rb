03e0e4572263f065fa156fc1b280b352312a848d
Amandeep Singh <amandeep511997@gmail.com>
2017-11-28 16:47:13 +0530

    Redirect user back to the projects page if they try going to a project that does not exist

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,6 +8,8 @@
 
 class ProjectsController < ApplicationController
 	
+	before_action :set_project, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@projects = Project.all
 	end
@@ -29,15 +31,14 @@ class ProjectsController < ApplicationController
 	end
 
 	def show
-		@project = Project.find(params[:id])
+		
 	end
 
 	def edit 
-		@project = Project.find(params[:id])
+		
 	end 
 
 	def update
-		@project = Project.find(params[:id])
 		
 		if @project.update(project_params)
 			flash[:notice] = "Project has been updated."
@@ -49,7 +50,7 @@ class ProjectsController < ApplicationController
 	end
 
 	def destroy 
-		@project = Project.find(params[:id])
+
 		@project.destroy
 
 		flash[:notice] = "Project has been deleted."
@@ -61,6 +62,16 @@ private
 
 	def project_params
 		params.require(:project).permit(:name, :description)
-	end	
+	end
+
+	def set_project
+		@project = Project.find(params[:id])
+
+	rescue ActiveRecord::RecordNotFound
+		flash[:alert] = "The project you were looking for could not be found."
+		redirect_to projects_path
+
+	end 
+
 	
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
+	it "handles a missing project correctly" do
 
+		# ** since this is test for projects controller
+		# the :show action will be used of projects
+		get :show, id: "not-here"
+
+		expect(response).to redirect_to(projects_path)
+
+		message = "The project you were looking for could not be found."
+		expect(flash[:alert]).to eq message
+		
+	end
 end
