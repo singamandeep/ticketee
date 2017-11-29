af1c2d3848b942d0292255de6d40d9433646d5ff
Amandeep Singh <amandeep511997@gmail.com>
2017-11-29 10:09:40 +0530

    Implement ticket display

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,6 @@
 class Ticket < ApplicationRecord
 	belongs_to :project
 
-	validates :name, presence: true
-	validates :description, presence: true, length: {minimum: 10}
+	validates :name, :description, presence: true
+	validates :description, length: {minimum: 10}
 end
---
+++ spec/features/viewing_tickets_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.feature "User can view tickets" do
+	scenario "for a given project" do
+		# ** arrange
+		sublime = FactoryGirl.create(:project, name: "Sublime Text 3")
+		FactoryGirl.create(:ticket, project: sublime, name: "Make it shiny!", description: "Gradients! Starbursts! Oh my!")
+		ie = FactoryGirl.create(:project, name: "Internet Explorer")
+		FactoryGirl.create(:ticket, project: ie, name: "Standard Compilance", description: "Isn't a joke!")
+
+		# ** act 
+		visit '/'
+		click_link "Sublime Text 3"
+
+
+		# ** assert
+		expect(page).to have_content "Make it shiny!"
+		expect(page).to_not have_content "Standard Compilance"
+		click_link "Make it shiny!"
+		# ** this means on the current loaded page find a div with 
+		# ** id = "ticket" and in that div find a tag h2 within 
+		# ** which the content should be present.
+		within('#ticket h2') do
+			expect(page).to have_content "Make it shiny!"
+		end
+		expect(page).to have_content "Gradients! Starbursts! Oh my!"
+	end
+end
