f3d0b1d3a7ab08f6e0b654a49c179fa17f437959
Amandeep Singh <amandeep511997@gmail.com>
2017-11-29 14:14:58 +0530

    Styled sign up and sign in forms

--- spec/features/signing_in_spec.rb
+++ spec/features/signing_in_spec.rb
@@ -4,12 +4,12 @@ RSpec.feature "User can sign in" do
 	let!(:user) { FactoryGirl.create(:user) }
 
 	scenario "with valid credentials" do
-		
+
 		visit "/"
 		click_link "Sign in"
 		fill_in "Email", with: user.email
 		fill_in "Password", with: "password"
-		click_button "Log in"
+		click_button "Sign in"
 
 		expect(page).to have_content "Signed in successfully"
 		expect(page).to have_content "Signed in as #{user.email}"
