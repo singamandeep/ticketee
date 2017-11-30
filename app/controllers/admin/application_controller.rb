9da6d8bdd184e8787481bbe46d301cf6af368d90
Amandeep Singh <amandeep511997@gmail.com>
2017-11-30 13:11:31 +0530

    Ony admins can see the links to create and delete projects

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -8,7 +8,10 @@ private
 	def authorize_admin!
 		authenticate_user! # ** given by Devise
 
-		unless current_user.admin?
+		# ** the admin method comes from the 
+		# ** attributes set in the database and 
+		# ** it exists for each attribute of the database
+		unless current_user.admin? 
 			redirect_to root_path
 			flash[:alert] = "You must be an admin to do that."
 		end
---
+++ spec/features/hidden_links_spec.rb
@@ -0,0 +1,51 @@
+require "rails_helper"
+
+RSpec.feature "Users can only see the appropriate links" do
+	let(:user) { FactoryGirl.create(:user) }
+	let(:admin) { FactoryGirl.create(:user, :admin) }
+	let(:project) { FactoryGirl.create(:project) }
+
+	context "anonymous users" do
+		scenario "cannot see the New Project link" do
+			visit "/"
+			expect(page).not_to have_link "New Project"
+		end
+
+		scenario "cannot see the Delete Project link" do
+			visit project_path(project)
+			expect(page).not_to have_link "Delete Project"
+		end
+	end
+
+	context "regular users" do
+		before do
+			login_as(user)
+		end	
+
+		scenario "cannot see the New Project link" do
+			visit "/"
+			expect(page).not_to have_link "New Project"
+		end
+
+		scenario "cannot see the Delete Project link" do
+			visit project_path(project)
+			expect(page).not_to have_link "Delete Project"
+		end
+	end
+
+	context "admin users" do
+		before do
+			login_as(admin)
+		end	
+
+		scenario "can see the New Project link" do
+			visit "/"
+			expect(page).to have_link "New Project"
+		end
+
+		scenario "can see the Delete Project link" do
+			visit project_path(project)
+			expect(page).to have_link "Delete Project"
+		end
+	end	
+end
