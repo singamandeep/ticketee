7cccac4c933a082d96da8110bf57683c160a60a7
Amandeep Singh <amandeep511997@gmail.com>
2017-12-01 22:26:36 +0530

    Added permissions for editing tickets

--- app/policies/ticket_policy.rb
+++ app/policies/ticket_policy.rb
@@ -13,4 +13,8 @@ class TicketPolicy < ApplicationPolicy
   def create?
   	user.try(:admin?) || record.project.has_manager?(user) || record.project.has_editor?(user)
   end
+
+  def update?
+    user.try(:admin?) || record.project.has_manager?(user) || (record.project.has_editor?(user) && record.author == user)
+  end
 end
--- spec/policies/ticket_policy_spec.rb
+++ spec/policies/ticket_policy_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe TicketPolicy do
 
       it { should_not permit_action :show}
       it { should_not permit_action :create}
+      it { should_not permit_action :update}
     end
 
     context "for viewers of the project" do
@@ -22,6 +23,7 @@ RSpec.describe TicketPolicy do
 
       it { should permit_action :show}
       it { should_not permit_action :create}
+      it { should_not permit_action :update}
     end
 
     context "for editors of the project" do
@@ -29,6 +31,12 @@ RSpec.describe TicketPolicy do
 
       it { should permit_action :show}
       it { should permit_action :create}
+      it { should_not permit_action :update}
+      context "when the editor created the ticket" do
+        before { ticket.author = user }
+
+        it { should permit_action :update}
+      end
     end
 
     context "for managers of the project" do
@@ -36,6 +44,7 @@ RSpec.describe TicketPolicy do
 
       it { should permit_action :show}
       it { should permit_action :create}
+      it { should permit_action :update}
     end
 
     context "for managers of other projects" do
@@ -46,6 +55,7 @@ RSpec.describe TicketPolicy do
 
       it { should_not permit_action :show}
       it { should_not permit_action :create}
+      it { should_not permit_action :update}
     end
 
     context "for administrators" do
@@ -53,6 +63,7 @@ RSpec.describe TicketPolicy do
 
       it { should permit_action :show}  
       it { should permit_action :create}
+      it { should permit_action :update}
     end
 
   end
