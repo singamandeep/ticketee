d5aa57d767bb099d3f404c64b113793274363d52
Amandeep Singh <amandeep511997@gmail.com>
2017-12-01 13:27:11 +0530

    Handled authorization errors gracefully

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
 	include Pundit
 
 	protect_from_forgery with: :exception
+
+	# ** its a global rescue_from to rescue from Pundit errors.
+	rescue_from Pundit::NotAuthorizedError, with: :not_authorized
+
+private 
+
+	def not_authorized
+		flash[:alert] = "You aren't allowed to do that."
+		redirect_to root_path
+	end
+
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe ProjectsController, type: :controller do
 		expect(flash[:alert]).to eq message
 		
 	end
+
+	# ** we just do a fake login as warden cant be used here and
+	# ** then we click on project link to handle the "red page".
+	it "handles permission errors by redirecting to a safe place" do
+		allow(controller).to receive(:current_user)
+
+		project = FactoryGirl.create(:project)
+		get :show, id: project
+
+		expect(response).to redirect_to(root_path)
+		message = "You aren't allowed to do that."
+		expect(flash[:alert]).to eq message
+	end
 end
