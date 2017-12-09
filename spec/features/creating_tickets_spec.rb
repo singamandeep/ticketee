a6361c587b8eda1a2a6206320dfd9ffdb781ac5d
Amandeep Singh <amandeep511997@gmail.com>
2017-12-09 12:07:05 +0530

    File uploads are persisted across form re-displays

--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -57,18 +57,17 @@ RSpec.feature "User can create new tickets" do
 		end
 	end
 
-	# ** solving the problem - that if the form to create if rendered again 
-	# ** then the file should be retained.
 	scenario "persisting file uploads across form displays" do
 		attach_file "File", "spec/fixtures/speed.txt"
 		click_button "Create Ticket"
 
 		fill_in "Name", with: "Add documentation for blink tag"
-		fill_in "Description", with: "The blink tag has a speed attribute" 
+		fill_in "Description", with: "The blink tag has a speed attribute"
 		click_button "Create Ticket"
 
 		within("#ticket .attachment") do
 			expect(page).to have_content "speed.txt"
 		end
 	end
+
 end
