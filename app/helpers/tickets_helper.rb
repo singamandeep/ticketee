b7c4c6a6c706c3637e633c7ab411d41cf2f13720
Amandeep Singh <amandeep511997@gmail.com>
2018-04-05 21:00:37 +0530

    Added watch and unwatch icons

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -11,8 +11,9 @@ module TicketsHelper
 	end
 
 	def toogle_watching_button(ticket)
-		text = (ticket.watchers.include?(current_user)) ? "Unwatch" : "Watch"
+		text = (ticket.watchers.include?(current_user)) ? "<i class='fa fa-eye-slash' aria-hidden='true'></i> Unwatch" : "<i class='fa fa-eye' aria-hidden='true'></i>
+ Watch"
 		# we used post method since we are going to create a new watcher
-		link_to text, watch_project_ticket_path(ticket.project, ticket), class: text.parameterize, method: :post
+		link_to text.html_safe, watch_project_ticket_path(ticket.project, ticket), class: "btn btn-primary", method: :post
 	end
 end
